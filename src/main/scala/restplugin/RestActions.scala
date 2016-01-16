cea996c38ad4d1ae8e140a5d88c303163d143f6f
Johannes Unterstein <unterstein@me.com>
2016-01-16 22:18:24 +0100

    use serializer

--- src/main/scala/restplugin/RestActions.scala
+++ src/main/scala/restplugin/RestActions.scala
@@ -7,9 +7,16 @@ import play.api.mvc._
  */
 trait RestActions extends Controller {
 
-
-  def RESTfulAction()(f: Request[AnyContent] => Result) = Action {
+  def RESTfulAction[T](requiredClass: Class[T], objectRequired: Boolean = false)(f: RESTfulRequest[AnyContent, _ >: T] => Result) = Action {
     implicit request =>
-      f(request)
+      val givenObject = Serializer.fromRequest(request, requiredClass)
+      if (objectRequired && givenObject.isEmpty) {
+        BadRequest("Entity missing")
+      } else {
+        f(RESTfulRequest(givenObject, request))
+      }
   }
+
+  case class RESTfulRequest[A, C](givenObject: Option[C], request: Request[A]) extends WrappedRequest[A](request)
+
 }
