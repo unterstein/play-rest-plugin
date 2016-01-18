09dc5954a18a03309af79cf3478c9403122c7ee0
Johannes Unterstein <unterstein@me.com>
2016-01-18 10:47:50 +0100

    renamed request class

--- src/main/scala/restplugin/RestActions.scala
+++ src/main/scala/restplugin/RestActions.scala
@@ -7,16 +7,16 @@ import play.api.mvc._
  */
 trait RestActions extends Controller {
 
-  def RESTfulAction[T](requiredClass: Class[T], objectRequired: Boolean = false)(f: RESTfulRequest[AnyContent, _ >: T] => Result) = Action {
+  def RESTfulAction[T](requiredClass: Class[T], objectRequired: Boolean = false)(f: RESTRequest[AnyContent, _ >: T] => Result) = Action {
     implicit request =>
       val givenObject = Serializer.fromRequest(request, requiredClass)
       if (objectRequired && givenObject.isEmpty) {
         BadRequest("Entity missing")
       } else {
-        f(RESTfulRequest(givenObject, request))
+        f(RESTRequest(givenObject, request))
       }
   }
 
-  case class RESTfulRequest[A, C](givenObject: Option[C], request: Request[A]) extends WrappedRequest[A](request)
+  case class RESTRequest[A, C](givenObject: Option[C], request: Request[A]) extends WrappedRequest[A](request)
 
 }
