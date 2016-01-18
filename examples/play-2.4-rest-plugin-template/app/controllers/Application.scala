aafe103c5adb00bad157727300ff2918e56ba6f4
Johannes Unterstein <unterstein@me.com>
2016-01-18 12:54:21 +0100

    more examples

--- examples/play-2.4-rest-plugin-template/app/controllers/Application.scala
+++ examples/play-2.4-rest-plugin-template/app/controllers/Application.scala
@@ -6,7 +6,12 @@ class Application extends RestActions {
 
   def index = RESTAction() {
     implicit request =>
-      Ok("ok: " + request.givenObject)
+      Ok(s"ok: $request.givenObject")
+  }
+
+  def examplePut(id: Long) = RESTAction() {
+    implicit request =>
+      Ok(s"ok: $id $request.givenObject")
   }
 
 }
